c77a66fa38e0d77ef3850f4fdb148b7d75895c2d
vaish8112006 <vaishnavinandhakumar6@gmail.com>
2025-12-28 14:29:09 +0530

    Delete BusinessClass.java
    Delete message

--- BusinessClass.java
+++
@@ -1,8 +0,0 @@
-package FlightSystem;
-
-public class BusinessClass extends FlightClass implements BusinessClassInterface{
-
-    public void accessLounge(){
-        System.out.println("Lounge Access Granted");
-    }
-}
